cebd43b77526d79a136ea8bad1c2ce5d31293535
ganeshsubramani7 <ganesh.subramani7@hotmail.com>
2018-08-29 17:27:43 +0530

    Changes Made

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
  
-   response.writeHead(200, {"Content-Type": "text/plain"});
-   
- response.end("Hello World!");
+response.writeHead(200, {"Content-Type": "text/plain"});
+  
+ 
+response.end("Welcome to Azure Web App!");
 
 });
+
+
 
 
 var port = process.env.PORT || 1337;
@@ -16,6 +19,4 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 
-/*console.log("Server running at http://localhost:%d", port);
-*/
-console.log("Hello");
+console.log("Server is running");
