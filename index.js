b074118e7829cf950f582761b8bcbcc61e93b337
ganeshsubramani7 <ganesh.subramani7@hotmail.com>
2018-09-10 14:01:12 +0530

    changes made in index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 
 var server = http.createServer(function(request, response) {
 
  
-response.writeHead(200, {"Content-Type": "text/plain"});
-  
- 
-response.end("Welcome to first Azure Web Application");
+response.writeHead(200, {"Content-Type": "text/html"});
+fs.readFile('./index.html', null, function(error, data) {
+   if(error) {
+	 response.writeHead(404);  
+	 response.write('File not found');
+   } else {
+	  response.write(data); 
+   }  
+response.end();
 
+});	
 });
 
 
